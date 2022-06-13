41ec9821c8615816ce2d77d662179f0d280851fc
SvetlanaVaskiv <svetanochka@gmail.com>
2022-06-13 12:29:09 +0200

    Added

--- server/routes/basketRouter.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express')
-const router = new Router
-const basketController = require('../controllers/basketController')
-const authMiddleware = require('../middleware/authMiddleware')
-
-
-
-router.get('/',authMiddleware, basketController.getOne)
-module.exports = router
