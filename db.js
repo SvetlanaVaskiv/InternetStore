4229d8f55ba732fd66be6d0c7a78bce2e59010bf
SvetlanaVaskiv <svetanochka@gmail.com>
2022-09-02 18:10:21 +0200

    ssl

--- db.js
+++ db.js
@@ -3,13 +3,13 @@ module.exports =
   process.env.NODE_ENV === "production"
     ? new Sequelize(process.env.DATABASE_URL)
     : new Sequelize(
-        process.env.DB_NAME || "dfbbhnmjfvgnft",
-        process.env.DB_USER || "hndywujhwacohe",
+        process.env.DB_NAME || "elitypescript",
+        process.env.DB_USER || "eli",
         process.env.DB_PASSWORD,
         {
           dialect: "postgres",
-          host: process.env.PSQL_HOST || "localhost",
-          // port: process.env.DB_PORT,
+          host: process.env.DB_HOST || process.env.PSQL_HOST ,
+          port: process.env.DB_PORT,
           dialect: "postgres",
           pool: {
             max: 100,
