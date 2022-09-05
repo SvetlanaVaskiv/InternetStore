0fcd0ba0d49e4a0e98f2e2ab78e02a5aa3102297
SvetlanaVaskiv <svetanochka@gmail.com>
2022-09-05 18:27:00 +0200

    routing

--- index.js
+++ index.js
@@ -21,18 +21,17 @@ const PORT= process.env.PORT || 8000
 const app = express()
 app.use(cors())
 app.use(express.json())
-app.use(express.static(path.resolve(__dirname, 'static')))
+//app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
 
-if (process.env.NODE_ENV === "production") {
   // Exprees will serve up production assets
-  app.use(express.static(path.join(__dirname, "build")));
+  app.use(express.static(path.join(__dirname, "client/build")));
 
   // Express serve up index.html file if it doesn't recognize route
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
-}
+
 
 app.use('/api', router)
 
