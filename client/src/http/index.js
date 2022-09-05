deb88d14f27675649e71266233b53528bb6ebdfd
SvetlanaVaskiv <svetanochka@gmail.com>
2022-09-05 18:50:25 +0200

    change axios url

--- client/src/http/index.js
+++ client/src/http/index.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const $host = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: 'https://svitlana-store.herokuapp.com/',
 });
 const $authHost = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: "https://svitlana-store.herokuapp.com/",
 });
 const authInterceptor = (config) => {
   config.headers.authorization = `Bearer ${localStorage.getItem("token")}`;
