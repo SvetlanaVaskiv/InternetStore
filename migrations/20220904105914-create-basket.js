7915bc139b44e098165b068f140c7b42c599977c
SvetlanaVaskiv <svetanochka@gmail.com>
2022-09-27 12:23:55 +0200

    test

--- migrations/20220904105914-create-basket.js
+++ migrations/20220904105914-create-basket.js
@@ -16,9 +16,17 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE,
       },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "User",
+          key: "id",
+        },
+      },
     });
     //basket belongsTo user
-    await queryInterface.addColumn("baskets", "userId", {
+   /* await queryInterface.addColumn("baskets", "userId", {
       type: Sequelize.INTEGER,
       references: {
         model: "users",
@@ -26,7 +34,7 @@ module.exports = {
       },
       onUpdate: "CASCADE",
       onDelete: "SET NULL",
-    });
+    });*/
     //basket hasMany basket_device
    /*await queryInterface.addColumn("basket_devices", "basket_id", {
       type: Sequelize.INTEGER,
